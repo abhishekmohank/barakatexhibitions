4afde667ac74f586c2aa1a8bdb524108f8e91c00
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-06-03 20:18:31 +0530

    remove image

--- src/constants/galleryimages.js
+++ src/constants/galleryimages.js
@@ -1,4 +1,3 @@
-import gvindia from "../assets/images/gvindia.jpg";
 import gvnight1 from "../assets/images/gvnight1.jpg";
 import gvnight2 from "../assets/images/gvnight2.jpg";
 import gvnight3 from "../assets/images/gvnight3.jpg";
@@ -30,50 +29,46 @@ const Images = [
   },
   {
     index: 4,
-    src: gvindia,
-  },
-  {
-    index: 5,
     src: gvnight1,
   },
   {
-    index: 6,
+    index: 5,
     src: gvnight2,
   },
   {
-    index: 7,
+    index: 6,
     src: gvnight3,
   },
   {
-    index: 8,
+    index: 7,
     src: gvnight4,
   },
   {
-    index: 9,
+    index: 8,
     src: gvnight5,
   },
   {
-    index: 10,
+    index: 9,
     src: gallery4,
   },
   {
-    index: 11,
+    index: 10,
     src: gallery5,
   },
   {
-    index: 12,
+    index: 11,
     src: gallery6,
   },
   {
-    index: 13,
+    index: 12,
     src: gallery7,
   },
   {
-    index: 14,
+    index: 13,
     src: gallery8,
   },
   {
-    index: 15,
+    index: 14,
     src: gallery9,
   },
 ];
