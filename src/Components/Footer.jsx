23b6d7f4918466cf921a78d944558e7090afac59
Abhishek <abhishekmohan119@gmail.com>
2024-08-16 15:49:28 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <div className="bg-gray-900 text-white">
-      <div className="container mx-auto gap-8 px-4 py-8 flex flex-col md:flex-row justify-center items-center">
+      <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0 md:space-x-8">
         <a
           target="_blank"
           href="https://maps.app.goo.gl/kex1Ed72K5aquwGU7?g_st=iw"
-          className="flex items-center mb-4 md:mb-0"
+          className="flex items-center"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -30,10 +30,10 @@ const Footer = () => {
           </svg>
           <div>
             <h1 className="font-semibold">Visit</h1>
-            <p>Dubai -UAE</p>
+            <p className="text-sm">Shams Al Barakat Exhibitions LLC Office: M-09 Arabilla Building, 15C Street, Hor Al Anz East, Dubai -UAE</p>
           </div>
         </a>
-        <div className="flex items-center mb-4 md:mb-0">
+        <div className="flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -50,12 +50,12 @@ const Footer = () => {
               d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
             />
           </svg>
-          <div className="flex md:flex-col flex-row">
+          <div>
             <h1 className="font-semibold">Mobile:</h1>
-            <p>+97150-3545972</p>
+            <p className="text-sm">+97150-3545972</p>
           </div>
         </div>
-        <div className="flex items-center mb-4 md:mb-0">
+        <div className="flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -74,7 +74,7 @@ const Footer = () => {
           </svg>
           <div>
             <h1 className="font-semibold">Email:</h1>
-            <p>info@barakatexhibitions.com</p>
+            <p className="text-sm">info@barakatexhibitions.com</p>
           </div>
         </div>
         <div className="flex items-center">
@@ -91,7 +91,7 @@ const Footer = () => {
             <path d="M244.036,217.014c-11.737,20.141-33.562,32.635-56.956,32.635c-36.329,0-65.921-29.585-65.921-65.915 c0-36.36,29.592-65.955,65.921-65.955c23.395,0,45.219,12.54,56.956,32.641l1.517,2.627h44.28l-2.658-7.129 c-7.705-20.413-21.225-37.769-39.122-50.157c-17.942-12.42-39.017-19.009-60.973-19.009c-58.981,0-106.946,48.006-106.946,106.982 c0,58.98,47.965,106.941,106.946,106.941c21.956,0,43.03-6.567,60.973-19.006c17.897-12.391,31.417-29.741,39.122-50.154 l2.658-7.133h-44.28L244.036,217.014z"></path>{" "}
           </svg>
           <div>
-            <h1 className="font-semibold">2024 by barakatexhibitions</h1>
+            <h1 className="font-semibold text-sm">2024 by barakatexhibitions</h1>
           </div>
         </div>
       </div>
